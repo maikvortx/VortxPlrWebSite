f306f77e3b2be5aba48a9961c84bcd41a73a05b9
Maik Braga Moura <65735194+maikvortx@users.noreply.github.com>
2022-06-13 00:30:03 -0300

    feat: remove nossos numeros da home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Faq from '../components/Home/FAQ/Faq'
 import Banner from './../components/Home/Banner/Banner'
-import NossosNumeros from '../components/Home/NossosNumeros'
+// import NossosNumeros from '../components/Home/NossosNumeros'
 import Institucional from '../components/Home/Institucional'
 // import TopTokens from '../components/Home/TopTokens'
 
